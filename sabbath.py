4e6b173d7bbefd4e138e33026498564503925216
Johan Thorén <johan@thoren.xyz>
2018-01-01 21:30:21 +0100

    Added additional strings to the example output at the end.

--- sabbath.py
+++ sabbath.py
@@ -195,21 +195,31 @@ if __name__ == '__main__':
         logging.debug('Creating object %s' % location)
         location.weekly_sabbath()
         print('The chosen location is {}'.format(location.city_name))
-        print('The time in {} is now {}'.format(location.city_name,
-                                                location.current_time))
         print('The gregorian date in {} is now {}'.format(
             location.city_name, location.current_date))
+        print('The time in {} is now {}'.format(location.city_name,
+                                                location.current_time))
         if location.sun_has_set is True:
             print('The sun is down')
             print('The sunset was at {}'.format(location.sunset_time))
         if location.sun_has_risen is False:
             print('The sun has not yet risen')
             print('The sunrise will be at {}'.format(location.sunrise_time))
-        print('Today is the {} day of the week.'.format(location.weekday))
+        # If sun_has_set is None, it should be in the morning. Therefor, check
+        # if the sun has risen.
+        if location.sun_has_set is None and location.sun_has_risen is True:
+            print('The sun is still up')
+            print('The sunset will be at {}'.format(location.sunset_time))
+        # If sun_has_risen is None it should be in the afternoon. Therefor,
+        # check if the sun has set.
+        if location.sun_has_risen is None and location.sun_has_set is False:
+            print('The sun is still up')
+            print('The sunset will be at {}'.format(location.sunset_time))
+        print('Today is the {} day of the week'.format(location.weekday))
         if location.sabbath is True:
             if location.is_ws is True:
                 print('It is now the weekly Sabbath')
             elif location.is_hfd is True:
                 print('It is now a High Feast day, and therefor a Sabbath')
             else:
-                print('Error: Unkown Sabbath.')
+                print('Error: Unkown Sabbath')

29cc3265e10a98cb124c24fc04a8b3f87df49fda
Johan Thorén <johan@thoren.xyz>
2018-01-05 14:34:11 +0100

    Reorganized the classes and refined some functions.

--- aviv/test_basic.py
+++ aviv/test_basic.py
@@ -4,10 +4,27 @@ import core
 import hist_data
 
 
-def test_days_of_month():
+def test_estimated_days_of_month():
     '''BibMonth should give months with 28-30 days'''
-    for key, value in hist_data.known_moons.items():
-        month = core.month_from_key(key)
+    for key, value in hist_data.estimated_moons.items():
+        month = core.bibmonth_from_key(key)
         result = month.length
         if result is not None:
             assert 28 <= result <= 30
+
+
+def test_known_days_of_month():
+    '''BibMonth should give months with 28-30 days'''
+    for key, value in hist_data.known_moons.items():
+        month = core.bibmonth_from_key(key)
+        result = month.length
+        if result is not None:
+            assert 28 <= result <= 30
+
+
+def test_estimated_moon_is_known():
+    '''Months should be moved from the estimated_moons dict when
+    observation is available'''
+    for key, value in hist_data.estimated_moons.items():
+        assert key not in hist_data.known_moons.keys(
+        ), 'the key {} is a duplicate'.format(key)

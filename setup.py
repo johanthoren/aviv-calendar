33805ee0cca9e3c357292ecd2c2877eea5148b8c
Johan Thorén <johan@thoren.xyz>
2018-02-08 11:42:42 +0100

    Moved the files around a bit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='Aviv Calendar',
+      version='0.1.1',
+      description='Biblical calendar tool',
+      author='Johan Thorén',
+      author_email='johan@avivcalendar.com',
+      url='https://www.avivcalendar.com'
+      packages=['aviv],
+      )

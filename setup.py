824ea5e73c09ae63aeede606483dff983cec2dc0
Johan Thorén <johan@thoren.xyz>
2018-02-12 19:42:25 +0100

    Added long description to setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
+from codecs import open
+from os import path
 from setuptools import setup, find_packages
+
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    LONG_D = f.read()
 
 setup(
     name='aviv',
     version='0.1.3',
     description='Biblical calendar tool',
+    long_description=LONG_D,
     author='Johan Thorén',
     author_email='johan@avivcalendar.com',
     license='GPLv2',
     url='https://www.avivcalendar.com',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=['astral'],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
@@ -26,6 +35,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
 )

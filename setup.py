153841477d5d7e26793672ab9be93a900367a393
Johan Thorén <johan@thoren.xyz>
2018-02-18 02:05:53 +0100

    New version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aviv',
-    version='0.1.4a',
+    version='0.1.5',
     description='Biblical calendar tool',
     author='Johan Thorén',
     author_email='johan@avivcalendar.com',

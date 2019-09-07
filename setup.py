63b5bcdb3e74ff3a95c9be27de7dd91df9d4cf40
Johan Thoren <jthoren@op5.com>
2019-09-07 13:34:36 +0200

    Updated and released version 0.1.19

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aviv',
-    version='0.1.17',
+    version='0.1.19',
     description='Biblical calendar tool',
     author='Johan Thorén',
     author_email='johan@avivcalendar.com',
